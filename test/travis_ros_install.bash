367b2f87fec668fc3eda99908785b32af8ec8109
Yuichi Koshihara <yuichi.koshihara@gmail.com>
2019-02-11 09:59:47 +0900

    revised5

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,9 +8,7 @@ sudo pip install rospkg
 
 #ros install
 cd ..
-#git clone https://github.com/YuichiKoshihara/ros_setup_scripts_Ubuntu16.04_server.git
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
-#cd ./ros_setup_scripts_Ubuntu16.04_server
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
@@ -18,7 +16,6 @@ bash ./step1.bash
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-#source ~/catkin_ws/devel/setup.bash
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
